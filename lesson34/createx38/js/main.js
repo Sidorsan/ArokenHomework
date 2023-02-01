fd9f2d4b18b0e55e8ee11cec127b83a3f18e34c6
Sidor007 <Sidor007@yandex.ru>
2023-02-01 17:41:54 +0700

    lesson38

--- lesson34/createx38/js/main.js
+++ lesson34/createx38/js/main.js
@@ -4,5 +4,4 @@ $(function () {
     $('.directions__filter_btn').removeClass('directions__filter_btn--active')
     $(this).addClass('directions__filter_btn--active')
   })
-  $('.team__slider').slick({arrows: false, slidesToShow: 4, infiniti: true});
 })
